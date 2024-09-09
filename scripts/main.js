70d9f4207c72d70bea1feb97c81b9b1142b3da7f
Julian <62448764+OnkelJuls@users.noreply.github.com>
2024-09-09 14:46:28 +0200

    get the computers and humans choice

--- scripts/main.js
+++ scripts/main.js
@@ -1,1 +1,20 @@
 console.log("Hello World!");
+
+function getRandomInt(max) {
+  return Math.floor(Math.random() * max);
+}
+
+console.log(getRandomInt(3));
+
+function getHumanChoice(n) {
+  if (n == 0) {
+    console.log("You chose Rock!");
+  } else if (n == 1) {
+    console.log("You chose Paper!");
+  } else if (n == 2) {
+    console.log("You chose Scissors!");
+  } else {
+    console.log("enter a Valid option!");
+  }
+}
+getHumanChoice(prompt("Chose your option! 0: Rock; 1: Paper; 2: Scissors"));
