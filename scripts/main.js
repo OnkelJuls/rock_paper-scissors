7f1ea0b5e861d232c572590264e58836a18ad6e3
Julian <62448764+OnkelJuls@users.noreply.github.com>
2024-09-10 09:36:32 +0200

    add style and add prompt for rounds

--- scripts/main.js
+++ scripts/main.js
@@ -17,55 +17,79 @@ function getHumanChoice() {
   if (humanChoices.includes(humanChoice)) {
     return humanChoice;
   } else {
-    console.log("Please enter a valid Choice!");
+    console.log("%cPlease enter a valid Choice!", "color:red;");
   }
 }
 
 function playRound(humanChoice, computerChoice) {
   if (humanChoice === computerChoice) {
-    console.log(`It's a tie!`);
+    console.log(`%cIt's a tie!`, "color:blue;");
     return "tie";
   } else if (
     (humanChoice === "rock" && computerChoice === "paper") ||
     (humanChoice === "paper" && computerChoice === "scissors") ||
     (humanChoice === "scissors" && computerChoice === "rock")
   ) {
-    console.log(`Computer won!`);
+    console.log(`%cComputer won!`, "color:red;");
     return "computer";
-  } else {
-    console.log("human won!");
+  } else if (
+    (computerChoice === "rock" && humanChoice === "paper") ||
+    (computerChoice === "paper" && humanChoice === "scissors") ||
+    (computerChoice === "scissors" && humanChoice === "rock")
+  ) {
+    console.log("%cHuman won!", "color:green;");
     return "human";
+  } else {
+    return "invalid";
   }
 }
-let rounds = prompt("How many rounds dou you want to play?");
+
+let rounds = parseInt(
+  prompt("How many rounds do you want to play?(at least 2)")
+);
+
 function playGame() {
   let humanScore = 0;
   let computerScore = 0;
+  if (Number.isInteger(rounds) && rounds > 1) {
+    for (let i = 1; i <= rounds; i++) {
+      console.log(`%cRound: ${i} =>`, "color:orange;");
+      const roundResult = playRound(getComputerChoice(3), getHumanChoice());
 
-  for (let i = 1; i <= rounds; i++) {
-    console.log(`Round: ${i} =>`);
-    const roundResult = playRound(getComputerChoice(3), getHumanChoice());
-
-    if (roundResult === "human") {
-      humanScore++;
-    } else if (roundResult === "computer") {
-      computerScore++;
+      if (roundResult === "human") {
+        humanScore++;
+      } else if (roundResult === "invalid") {
+        console.log(
+          `%cRound ${i} is skipped due to an invalid input!`,
+          "color:red;font-weight: bold;"
+        );
+      } else if (roundResult === "computer") {
+        computerScore++;
+      }
+      console.log(
+        `%cResults after round ${i}:\nHuman: ${humanScore} Computer: ${computerScore}\n\n\n`,
+        "text-decoration: underline;"
+      );
     }
-    console.log(
-      `Results after round ${i}:\nHuman: ${humanScore} Computer: ${computerScore}\n\n\n`
-    );
-  }
 
-  if (humanScore > computerScore) {
-    console.log(
-      `Human won with ${humanScore} rounds! Computer got ${computerScore}.`
-    );
-  } else if (computerScore > humanScore) {
-    console.log(
-      `Computer won with ${computerScore} rounds! Human got ${humanScore}.`
-    );
+    if (humanScore > computerScore) {
+      console.log(
+        `%cHuman won with ${humanScore} round(s)! Computer got ${computerScore}.`,
+        "color:green;"
+      );
+    } else if (computerScore > humanScore) {
+      console.log(
+        `%cComputer won with ${computerScore} round(s)! Human got ${humanScore}.`,
+        "color:red;"
+      );
+    } else {
+      console.log(
+        `%cIts a Tie! Both got ${humanScore} round(s)`,
+        "color:blue;"
+      );
+    }
   } else {
-    console.log(`Its a Tie!`);
+    alert("Please reload and enter a number higher than 1");
   }
 }
 playGame();
